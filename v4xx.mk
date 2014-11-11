6425bb3672339da342d9e0f5e02d59ef71190c9d
Adam Lesinski <adamlesinski@google.com>
2014-11-11 11:12:01 -0800

    Remove density from PRODUCT_AAPT_CONFIG
    AAPT ignores densities in PRODUCT_AAPT_CONFIG.
    The use of PRODUCT_AAPT_PREF_CONFIG for density is
    encouraged, as AAPT is able to determine the fallback
    density to use if a resource of the specified density
    does not exist.
    
    Change-Id: If2b43a44593bc234118045d03bd48cd9820dc05a

--- v4xx.mk
+++ v4xx.mk
@@ -55,7 +55,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
 
 # Screen density
-PRODUCT_AAPT_CONFIG := normal large tvdpi hdpi
+PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := tvdpi
 
 # Boot animation
