65f561da1c01f166db6d96c0a522c6d3fbe755a8
Tim Brendel <timbrendelaz@gmail.com>
2015-03-08 09:58:04 -0700

    Change lcd_density

--- system_prop.mk
+++ system_prop.mk
@@ -31,7 +31,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.hwc.mdpcomp.enable=true \
     ro.opengles.version=196608 \
-    ro.sf.lcd_density=320
+    ro.sf.lcd_density=213
 
 # GPS
 PRODUCT_PROPERTY_OVERRIDES += \
