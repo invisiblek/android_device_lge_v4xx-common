a9e3cef9c49c02bb18f7faa86b8b8063a636a6a1
Dan Pasanen <dan.pasanen@gmail.com>
2015-03-14 13:44:38 -0500

    init: remove lg's charge_only stuff
    * we don't use this
    
    Change-Id: Icbe3b7504f7e3c49895ece587d477e5bffdb8e1c

--- rootdir/etc/init.zetaw.usb.sh
+++ rootdir/etc/init.zetaw.usb.sh
@@ -79,22 +79,6 @@ case "$usbcurrentlimit" in
 esac
 
 #
-# Allow USB enumeration with LGE default PID/VID
-#
-echo 1  > /sys/class/android_usb/f_mass_storage/lun/nofua
-usb_config=`getprop persist.sys.usb.config`
-case "$usb_config" in
-    #USB persist config not set, select default configuration
-    "")
-        setprop persist.sys.usb.config charge_only
-    ;;
-    "adb")
-        setprop persist.sys.usb.config charge_only,adb
-    ;;
-    * ) ;; #USB persist config exists, do nothing
-esac
-
-#
 # Add support for exposing lun0 as cdrom in mass-storage
 #
 target=`getprop ro.product.device`
