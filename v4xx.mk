0c55c30b9267311c27a9f8eba444804017f982a1
Bang Nguyen <bangprovn@gmail.com>
2015-03-16 12:39:38 -0500

    v4xx: move media_profiles to variant trees
    Change-Id: Ic58d2c545ab0ad616d7e4f0ea0e9e32f4a1b76bc

--- v4xx.mk
+++ v4xx.mk
@@ -133,8 +133,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
