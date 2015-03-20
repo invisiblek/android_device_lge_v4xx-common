13ad9418ab98ac139edf3b368ef9b22115224e01
Dan Pasanen <dan.pasanen@gmail.com>
2015-03-20 12:21:28 -0500

    v4xx: set aapt configs based on grouper
    Change-Id: Ibf314fd38f312d778520ab2c11aae4f605597ebd

--- v4xx.mk
+++ v4xx.mk
@@ -55,8 +55,8 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
 
 # Screen density
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
+PRODUCT_AAPT_CONFIG := normal large tvdpi hdpi
+PRODUCT_AAPT_PREF_CONFIG := tvdpi
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
