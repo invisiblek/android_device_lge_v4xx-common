079b0bc223b50e1bd842cd1022e6fc0a20acaab5
Sultanxda <sultanxda@gmail.com>
2015-03-27 02:39:01 -0700

    v4xx: Update WiFi supplicant overlays from hammerhead
    Change-Id: Ib6965a9da6e806974a8831f1a53c70a4295b8e2d

--- v4xx.mk
+++ v4xx.mk
@@ -222,7 +222,5 @@ PRODUCT_COPY_FILES += \
     device/lge/v4xx-common/wcnss/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     device/lge/v4xx-common/wcnss/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
     device/lge/v4xx-common/wcnss/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin \
-    device/lge/v4xx-common/wcnss/p2p_supplicant.conf:system/etc/wifi/p2p_supplicant.conf \
     device/lge/v4xx-common/wcnss/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
-    device/lge/v4xx-common/wcnss/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     device/lge/v4xx-common/wcnss/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
