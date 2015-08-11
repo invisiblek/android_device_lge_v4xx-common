fe491ec04851b7fbb6eff41aaf11cf3f45b2fb3f
Dan Pasanen <dan.pasanen@gmail.com>
2015-08-11 14:42:53 -0500

    mkbootimg: selectively bump images based on board flag
    Change-Id: I3b80a77d681e26b12efbbc9908ee34a9f8cb194f

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -18,7 +18,9 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTAL
 	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
 	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
+ifeq ($(TARGET_REQUIRES_BUMP),true)
 	$(hide) $(BUMP) $@ $@
+endif
 
 ## Overload recoveryimg generation: Same as the original, + --dt arg
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) \
@@ -28,4 +30,6 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) \
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
+ifeq ($(TARGET_REQUIRES_BUMP),true)
 	$(hide) $(BUMP) $@ $@
+endif
