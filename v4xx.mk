8631d6fe069570f0a1ece89cd5823abfec11527c
Adam Farden <adam@farden.cz>
2015-11-10 14:42:43 +0100

    v4xx-common: Use OSS TimeKeep service by Sony
    Change-Id: I7a25f6f82454912517aaa44c4804322b0c7e24d6

--- v4xx.mk
+++ v4xx.mk
@@ -50,6 +50,11 @@ PRODUCT_COPY_FILES += \
 -include $(LOCAL_PATH)/system_prop.mk
 PRODUCT_CHARACTERISTICS := tablet
 
+# Sony timekeep
+PRODUCT_PACKAGES += \
+    timekeep \
+    TimeKeep
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
