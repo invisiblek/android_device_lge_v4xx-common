e05788c9d4dba610c8a6f86264fd606d9d140269
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-05-04 07:21:32 -0500

    v4xx: update tap to wake node
    Change-Id: I452c872601c387e6bc7c1df0f576ed18f21b0977

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -70,7 +70,7 @@ COMMON_GLOBAL_CFLAGS += -DPROPERTY_PERMS_APPEND=' \
 
 # CMHW
 BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/cmhw/
-TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/lge_touch/lpwg_notify"
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/lge_touch/tap_to_wake"
 
 # Display
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
